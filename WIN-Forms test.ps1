a90ad9bd049d28ad93cf3634f22856b3b31f1833
Franklin <fl.tipton1@gmail.com>
2021-09-05 01:08:10 -0700

    Added button and function to copy files.

--- WIN-Forms test.ps1
+++ WIN-Forms test.ps1
@@ -1,16 +1,27 @@
-Add-type -assembly System.Windows.Forms
+# Load Assemblies
+Add-type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
-# Creating Variables 
+# Functions
+function Copy-Button {
+    Copy-Item "$InputBox" -Destination "$Outputbox"
+}
+
+# Main GUI appearance 
 $form = New-Object System.Windows.Forms.Form
-
-
-# Main GUI appearance
 $form.Text = 'GUI to transfer files from one machine to another'
-$form.Width = 500
-$form.Height = 350
+$form.Size = New-Object System.Drawing.Size(600,400)
 $form.AutoSize = $true
+$form.StartPosition = 'CenterScreen'
 
+# Create the Copy Button !! CHANGE LOCATION AND SIZE !!
+$CopyButton = New-Object System.Windows.Forms.Button
+$CopyButton.Location = New-Object System.Drawing.Size(20,90)
+$CopyButton.Size = New-Object System.Drawing.Size(200,20)
+$CopyButton.Text = "Copy Files"
+$CopyButton.Add_Click({Copy-Button})
 
+$form.controls.add($CopyButton)
 
 $form.ShowDialog()
 
